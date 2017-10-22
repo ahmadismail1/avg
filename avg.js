0ffe45ec3e5668d4d56502c41101186f8578f101
ahmadismail1 <32990146+ahmadismail1@users.noreply.github.com>
2017-10-23 00:44:12 +0300

    Add files via upload

---
+++ avg.js
@@ -0,0 +1,9 @@
+function avgg() {
+var m1 , m2 , m3 , m4 , m5 , m6 , m7 , h1 , h2 , h3 , h4 , h5 , h6 , h7 , avgn , ho , avg ; 
+var s = (m1*h1) + (m2*h2) + (m3*h3) + (m4*h4) + (m5*h5) + (m6*h6) + (m7*h7) ; 
+var d = avgn * ho; 
+var f = s + d 
+var avg = f / (h1 + h2 + h3 + h4 + h5 + h6 + h7 + ho ); 
+document.getElementById("demo").innerHTML = avg;
+alert (avg);
+}
